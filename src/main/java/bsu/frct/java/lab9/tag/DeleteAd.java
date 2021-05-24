126d586b503b6290d1ea63fb06df87f081a91ada
Timofey <froepamtima@gmail.com>
2021-05-24 06:59:04 +0300

    Deleting Added

---
+++ src/main/java/bsu/frct/java/lab9/tag/DeleteAd.java
@@ -0,0 +1,38 @@
+package main.java.bsu.frct.java.lab9.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import main.java.bsu.frct.java.lab9.entity.Ad;
+import main.java.bsu.frct.java.lab9.entity.AdList;
+import main.java.bsu.frct.java.lab9.entity.AdListHelper;
+import main.java.bsu.frct.java.lab9.entity.User;
+
+public class DeleteAd extends SimpleTagSupport {
+
+    private Ad ad;
+
+    public void setAd(Ad ad) {
+        this.ad = ad;
+    }
+
+    public void doTag() throws JspException, IOException {
+        String errorMessage = null;
+        AdList adList = (AdList) getJspContext().getAttribute("ads", PageContext.APPLICATION_SCOPE);
+        User currentUser = (User) getJspContext().getAttribute("authUser", PageContext.SESSION_SCOPE);
+
+        if (currentUser==null || (ad.getId()>0 && ad.getAuthorId()!=currentUser.getId())) {
+            errorMessage = "You are trying to change a message that you do not have permission to access!";
+        }
+
+        if (errorMessage==null) {
+            adList.deleteAd(ad);
+            AdListHelper.saveAdList(adList);
+        }
+
+        getJspContext().setAttribute("errorMessage", errorMessage,PageContext.SESSION_SCOPE);
+    }
+}
